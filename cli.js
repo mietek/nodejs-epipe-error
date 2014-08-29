23f9048f44959e2f506d8f468dd6d53d905ab7c5
Miëtek Bak <mietek@bak.io>
2014-08-29 02:15:36 +0100

    Catch and log spawn failures

--- cli.js
+++ cli.js
@@ -2,7 +2,14 @@ var util = require("util");
 var spawn = require("child_process").spawn;
 
 var doSpawn = function(callback){
-  var child = spawn('child.js');
+  var child = spawn('child.js')
+    .on('error', function (err) {
+      console.log(err.syscall + ' returned ' + err.errno);
+    });
+
+  child.stdin.on('error', function (err) {
+    console.log(err.syscall + ' returned ' + err.errno);
+  });
 
   child.on('exit', function(code){
     console.log("Child exited with code " + code);
